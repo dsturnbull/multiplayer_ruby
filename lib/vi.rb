3966a09aad61dec2439f6f50624ea594de8c37d0
Dave Turnbull <dave@redcordial.org>
2009-06-18 21:52:47 +1000

    add vim hints to temp files

--- lib/vi.rb
+++ lib/vi.rb
@@ -1,6 +1,9 @@
 require 'tempfile'
 
-@@temp = Tempfile.new('sirb_cli').path
+@@temp = Tempfile.new('sirb_cli')
+@@temp << "# vim:filetype=ruby\n\n"
+@@temp.read
+@@temp = @@temp.path
 
 def vi(file=nil)
   edit = Proc.new { |file| system("/usr/bin/env vim #{file}") }
@@ -11,5 +14,5 @@ end
 
 if $0 == __FILE__
   puts vi
-  puts vi 'example/text'
+  vi 'sirb.rb'
 end
